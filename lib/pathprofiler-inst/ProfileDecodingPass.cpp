0efd2da5d708a7473306498885d4d557efd7b441
roy <endvroy@gmail.com>
2017-10-01 17:55:01 -0700

    basic functions completed

--- lib/pathprofiler-inst/ProfileDecodingPass.cpp
+++ lib/pathprofiler-inst/ProfileDecodingPass.cpp
@@ -7,50 +7,128 @@
 #include "llvm/Support/FileSystem.h"
 
 #include <unordered_map>
-
+#include <queue>
 #include "ProfileDecodingPass.h"
-
+#include <unistd.h>
 
 using namespace llvm;
 using namespace pathprofiling;
 
 
 namespace pathprofiling {
-char ProfileDecodingPass::ID = 0;
+    char ProfileDecodingPass::ID = 0;
 }
 
+typedef std::pair<uint64_t, uint64_t> Info;
 
 // Given a sequence of basic blocks composing a path, this function prints
 // out the filename and line numbers associated with that path in CSV format.
 void
-printPath(llvm::raw_ostream& out, std::vector<llvm::BasicBlock*>& blocks) {
-  unsigned line = 0;
-  llvm::StringRef file;
-  for (auto* bb : blocks) {
-    for (auto& instruction : *bb) {
-      llvm::DILocation* loc = instruction.getDebugLoc();
-      if (loc && (loc->getLine() != line || loc->getFilename() != file)) {
-        line = loc->getLine();
-        file = loc->getFilename();
-        out << ", " << file.str() << ", " << line;
-      }
+printPath(FILE *f, std::vector<llvm::BasicBlock *> &blocks) {
+    unsigned line = 0;
+    llvm::StringRef file;
+    for (auto *bb : blocks) {
+        for (auto &instruction : *bb) {
+            llvm::DILocation *loc = instruction.getDebugLoc();
+            if (loc && (loc->getLine() != line || loc->getFilename() != file)) {
+                line = loc->getLine();
+                file = loc->getFilename();
+//                out << ", " << file.str() << ", " << line;
+                fprintf(f, ", %s, %u", file.str().c_str(), line);
+            }
+        }
     }
-  }
 }
 
 
 bool
 ProfileDecodingPass::runOnModule(Module &module) {
-  // Implement your solution here.
-  return false;
+    // Implement your solution here.
+    outs() << infilename.str().c_str();
+    auto inf = fopen(infilename.str().c_str(), "r");
+    uint64_t fnId, pathCode, count;
+    std::vector<uint64_t> countVec;
+    std::vector<Info> infoVec;
+
+    char *line = NULL;
+    size_t len = 0;
+    while (getline(&line, &len, inf) != -1) {
+        sscanf(line, "%lu %lu %lu", &fnId, &pathCode, &count);
+        printf("read line: %lu %lu %lu\n", fnId, pathCode, count);
+        countVec.push_back(count);
+        infoVec.emplace_back(fnId, pathCode);
+    }
+    fclose(inf);
+
+//    outs() << "\nreading file complete\n";
+
+    std::vector<int> toPrint;
+    for (int i = 0; i < countVec.size(); i++) {
+        toPrint.push_back(i);
+    }
+
+//    std::priority_queue<std::pair<double, int>> q;
+//    for (int i = 0; i < countVec.size(); i++) {
+//        q.push(std::pair<uint64_t, int>(countVec[i], i));
+//    }
+//    int k = 5; // number of indices we need
+//    for (int i = 0; i < k; ++i) {
+//        int ki = q.top().second;
+//        q.pop();
+//    }
+
+    auto of = stdout;
+    auto &ep = getAnalysis<PathEncodingPass>();
+    for (auto idx:toPrint) {
+        count = countVec[idx];
+        auto info = infoVec[idx];
+        fnId = info.first;
+        pathCode = info.second;
+        auto fn = ep.toInstr[fnId];
+        auto seq = decode(fn, pathCode);
+        fprintf(of, "%lu", count);
+        printPath(of, seq);
+        fprintf(of, "\n");
+    }
+    fclose(of);
+
+    return false;
 }
 
 
-std::vector<llvm::BasicBlock*>
-ProfileDecodingPass::decode(llvm::Function* function, uint64_t pathID) {
-  std::vector<llvm::BasicBlock*> sequence;
-  // You may want to implement and use this function as a part of your
-  // solution.
-  return sequence;
+std::vector<llvm::BasicBlock *>
+ProfileDecodingPass::decode(llvm::Function *function, uint64_t pathCode) {
+    std::vector<llvm::BasicBlock *> sequence;
+    // You may want to implement and use this function as a part of your
+    // solution.
+
+    using Edge = std::pair<llvm::BasicBlock *, llvm::BasicBlock *>;
+    auto &ep = getAnalysis<PathEncodingPass>();
+    auto &allEdges = ep.fnEdgeMap[function];
+
+    auto bb = &(function->getEntryBlock());
+    BasicBlock *next = nullptr;
+    while (true) {
+        sequence.push_back(bb);
+        uint64_t encoding = 0;
+        next = nullptr;
+        for (auto it = succ_begin(bb), et = succ_end(bb); it != et; ++it) {
+            auto succ = *it;
+            auto edgeCode = allEdges[Edge(bb, succ)];
+            if (edgeCode >= encoding && edgeCode <= pathCode) {
+                encoding = edgeCode;
+                next = succ;
+            }
+        }
+        if (next) {
+            pathCode -= encoding;
+            bb = next;
+        }
+        else {
+            break;  //no successor
+        }
+    }
+
+    return sequence;
 }
 
